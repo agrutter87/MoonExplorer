7c79e98121c58e2342769fedd0808979d56e2e70
agrutter87 <agrutter87@gmail.com>
2019-06-24 21:14:54 -0500

    Updated Rover
    Updated Rover scene to make RigidBody2D the root node of the scene.
    Corrected placement of sprites within scene tree.
    Updated Rover script to replace manual position control with physics control

--- Scripts/Rover.gd
+++ Scripts/Rover.gd
@@ -1,54 +1,60 @@
-extends Sprite
+extends RigidBody2D
 
-export var rover_speed = 10
+export var rover_acceleration = 10
+
+const TRACK_WIDTH = 24
+var left_track_accel = 0
+var right_track_accel = 0
 
 func _ready():
-	$LeftTrackAnimatedSprite.speed_scale = rover_speed
-	$RightTrackAnimatedSprite.speed_scale = rover_speed
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	var left_track_speed = 0
-	var right_track_speed = 0
-	$LeftTrackAnimatedSprite.speed_scale = rover_speed
-	$RightTrackAnimatedSprite.speed_scale = rover_speed
+	var new_left_accel = 0
+	var new_right_accel = 0
 	
 	# If any of the keys are pressed, update value
 	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
-		left_track_speed = left_track_speed - 1
-		right_track_speed = right_track_speed + 1
+		new_left_accel -= rover_acceleration
+		new_right_accel += rover_acceleration
 	if Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
-		left_track_speed = left_track_speed + 1
-		right_track_speed = right_track_speed - 1
+		new_left_accel += rover_acceleration
+		new_right_accel -= rover_acceleration
 	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
-		left_track_speed = left_track_speed + 1
-		right_track_speed = right_track_speed + 1
+		new_left_accel += rover_acceleration
+		new_right_accel += rover_acceleration
 	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
-		left_track_speed = left_track_speed - 1
-		right_track_speed = right_track_speed - 1
-	
+		new_left_accel -= rover_acceleration
+		new_right_accel -= rover_acceleration
+
+	left_track_accel = new_left_accel
+	right_track_accel = new_right_accel
+
 	# Update left track animation
-	if(left_track_speed > 0):
+	if(left_track_accel > 0):
 		$LeftTrackAnimatedSprite.play("driving")
-	elif(left_track_speed < 0):
+	elif(left_track_accel < 0):
 		$LeftTrackAnimatedSprite.play("driving", true)
 	else:
 		$LeftTrackAnimatedSprite.stop()
-	
+
 	# Update right track animation
-	if(right_track_speed > 0):
+	if(right_track_accel > 0):
 		$RightTrackAnimatedSprite.play("driving")
-	elif(right_track_speed < 0):
+	elif(right_track_accel < 0):
 		$RightTrackAnimatedSprite.play("driving", true)
 	else:
 		$RightTrackAnimatedSprite.stop()
 	
-	# Move rover depending on the left and right track behavior
-	translate(Vector2(0, -((right_track_speed + left_track_speed) * rover_speed * delta)).rotated(rotation))
-	rotate((left_track_speed - right_track_speed) * delta)
-
-
-func _on_RigidBody2D_body_entered(body):
-	$LeftTrackAnimatedSprite.stop()
-	$RightTrackAnimatedSprite.stop()
+	# Update track animation speed
+	$LeftTrackAnimatedSprite.speed_scale = linear_velocity.length() + abs(angular_velocity * (TRACK_WIDTH / 2))
+	$RightTrackAnimatedSprite.speed_scale = linear_velocity.length() + abs(angular_velocity * (TRACK_WIDTH / 2))
 	
+func _physics_process(delta):
+	# Move rover depending on the left and right track behavior	
+	apply_impulse(Vector2(0,0), (Vector2(0, -(left_track_accel + right_track_accel)).rotated(rotation)))
+	apply_torque_impulse(left_track_accel - right_track_accel)
+	
+func _on_Rover_body_entered(body):
+	print("Entered")
